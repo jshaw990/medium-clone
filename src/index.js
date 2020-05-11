b3ed0677561d0a42cd5cef97a42960137ea27384
Jayden Shaw <jayden.shaw@gmail.com>
2020-05-11 10:10:27 -0600

    index.js > SRC making redux store accessible for react components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(
-  <React.StrictMode>
+import App from './App.js';
+import serviceWorker from './serviceWorker';
+import { store, history } from './redux/store';
+ReactDOM.render((
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </Provider>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
