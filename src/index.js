315dc4f466ab52cb0685e0c153f7a1f642930f57
Jayden Shaw <jayden.shaw@gmail.com>
2020-05-11 11:58:15 -0600

    Added placeholder CSS. Fixed various typos

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/medium.css';
 import { Provider } from 'react-redux';
+
 import { Switch, Route } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
 
 import App from './App.js';
 import * as serviceWorker from './serviceWorker';
+
 import { store, history } from './redux/store';
 import { getUser } from './redux/actions/actions'
 
 if(localStorage.Auth) {
   store.dispatch({type: 'SET_USER', user: JSON.parse(localStorage.Auth)})
-  let _id = JSON.parse(localStorage.Auth)._id
+  var _id = JSON.parse(localStorage.Auth)._id
   getUser(_id).then((res) => {
     store.dispatch({type: 'SET_USER', user: res})
   })
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
 import reducer from './reducer';
 import thunk from 'redux-thunk'
-import createHistory from 'history/createbrowserHistory';
+import createHistory from 'history/createBrowserHistory';
 
 export const history = createHistory();
 
